83aba368a78864e5e833600258cbb2b091991851
jegasuguna <jegasuguna1996@gmail.com>
2022-07-15 16:17:41 -0400

    Company model

--- app/models.py
+++ app/models.py
@@ -1,8 +1,63 @@
+from email import contentmanager
+from enum import unique
 from flask_login import UserMixin
+from sqlalchemy import func
 from . import db
+from datetime import datetime
 
 class User(UserMixin, db.Model):
+    __tablename__ = "user"
     id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
     name = db.Column(db.String(1000))
+    roles = db.relationship('Role', secondary='user_roles')
+    company = db.relationship("Company", secondary="Members_Company")
+
+
+# Define the Role data-model
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(50), unique=True)
+
+# Define the UserRoles association table
+class UserRoles(db.Model):
+    __tablename__ = 'user_roles'
+    id = db.Column(db.Integer(), primary_key=True)
+    user_id = db.Column(db.Integer(), db.ForeignKey('user.id', ondelete='CASCADE'))
+    role_id = db.Column(db.Integer(), db.ForeignKey('roles.id', ondelete='CASCADE'))
+
+
+# copy/paste job from tobuwebflask per issue #39
+class BlogPost(db.Model):
+    __tablename__ = "blogpost"
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), unique=True)
+    date = db.Column(db.DateTime(timezone=True), server_default=func.now())
+    content = db.Column(db.String(10000))
+    tags = db.Column(db.String(1000))
+    # Logan Kiser: hold off on user/group field - something will be added when
+    #              we incorporate flask-principal
+    # user = db.Column(db.Integer)
+    # TODO
+    # Logan Kiser: might be nice to include a convenient __init__ method or two
+
+class Company(db.Model):
+    __tablename__ = "company"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=True)
+    address1 = db.Column(db.String())
+    address2 = db.Column(db.String())
+    city = db.Column(db.String())
+    state = db.Column(db.String())
+    zip = db.Column(db.Integer())
+    country = db.Column(db.String())
+    phone = db.Column(db.String(50), unique=True) 
+    email = db.Column(db.String(50), unique=True)
+    
+class Members_Company(db.Model):
+    __tablename__ = "members_company"
+    id = db.Column(db.Integer(), primary_key=True)
+    user_id= db.Column(db.Integer(), db.ForeignKey('User.id', ondelete='CASCADE'))
+    company_id = db.Column(db.Integer(), db.ForeignKey('company.id', ondelete='CASCADE'))
